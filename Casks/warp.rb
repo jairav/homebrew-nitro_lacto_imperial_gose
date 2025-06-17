fc9ed1e8c08dd7f7628b63929861e12d1e04404c
Jairav Desai <44884343+jairav@users.noreply.github.com>
2025-06-17 03:32:58 -0400

    Add warp cask and configuration

---
+++ Casks/warp.rb
@@ -0,0 +1,27 @@
+cask "warp" do
+  version :latest
+  sha256 :no_check
+
+  url "https://releases.warp.dev/stable/Warp.dmg"
+  name "Warp"
+  desc "Rust-based terminal with modern features"
+  homepage "https://www.warp.dev/"
+
+  auto_updates true
+
+  app "Warp.app"
+
+  postflight do
+    require "fileutils"
+    config_source = File.expand_path("../config/warp", __dir__)
+    config_target = File.expand_path("~/Library/Application Support/Warp", ENV["HOME"])
+    FileUtils.mkdir_p(config_target)
+    FileUtils.cp_r Dir[File.join(config_source, "*")], config_target, verbose: true
+  end
+
+  zap trash: [
+    "~/Library/Application Support/Warp",
+    "~/Library/Preferences/dev.warp.Warp.plist",
+    "~/Library/Saved Application State/dev.warp.Warp.savedState"
+  ]
+end
